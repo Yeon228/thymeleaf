da648b7a46b3efce49cfde158b59db1c3ec19738
doyeon <cumber88@naver.com>
2023-08-22 15:16:22 +0900

    검증 코드 ItemValidator로 모듈화

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +27,12 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;//@Componet로 등록을 해뒀기 때문에 자동으로 DI 시행
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder){
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -170,14 +179,15 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes
                           , Model model) {
-
+//      아래의 if 문을 한줄로 줄일 수 있으나, Empty, 공백 같은 단순한 조건만 검사 가능
+//        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
         //검증 로직
+
         if (!StringUtils.hasText(item.getItemName())){
             bindingResult.rejectValue("itemName", "required"); // errorCode에 input + obj Name + field Name이 됨
-
         }
         if(item.getPrice() == null || item.getPrice() < 1000 ||
                 item. getPrice() > 1000000){
@@ -200,6 +210,42 @@ public class ValidationItemControllerV2 {
         if(bindingResult.hasErrors()){
             log.info("errors = {} ", bindingResult);
 //            bindingResult는 자동으    로 모델에 담김
+//            model.addAttribute("errors", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+    //@PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes
+                          , Model model) {
+        itemValidator.validate(item, bindingResult);
+        //검증에 실패하면 다시 입력 폰으로
+        if(bindingResult.hasErrors()){
+            log.info("errors = {} ", bindingResult);
+//            bindingResult는 자동으    로 모델에 담김
+//            model.addAttribute("errors", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated  @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes
+            , Model model) {
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors = {} ", bindingResult);
+//            bindingResult는 자동으로 모델에 담김
 //            model.addAttribute("errors", bindingResult);
             return "validation/v2/addForm";
         }
